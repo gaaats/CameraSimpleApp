a357eb545df00a47abc30e3a351dd20e2b137a93
gaaats <mr.apemi@gmail.com>
2022-07-16 22:43:08 +0300

    done

--- app/src/main/java/com/example/camerasimpleapp/MainActivity.kt
+++ app/src/main/java/com/example/camerasimpleapp/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.camerasimpleapp
 import android.annotation.SuppressLint
 import android.media.Image
 import android.os.Bundle
+import android.util.Log
 import android.util.Size
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.example.camerasimpleapp.databinding.ActivityMainBinding
@@ -36,48 +39,32 @@ class MainActivity : AppCompatActivity() {
         ) {
             initCamera()
         }
-
-
     }
 
-    @SuppressLint("UnsafeOptInUsageError")
     private fun initCamera() {
         cameraProviderFuture.addListener({
-            val processCameraProvider = cameraProviderFuture.get()
-            // todo:for preview
-//            val preview = Preview.Builder().build()
-            // todo:for imageCapture
-//            val imageCapture = ImageCapture.Builder().build()
 
-            //задаємо приблизні параметри зобораження
-            val imageAnalysis = ImageAnalysis.Builder()
-                .setTargetResolution(Size(1024, 720))
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_BLOCK_PRODUCER)
-                .build()
+            // procces here
+            val processCameraProvider = cameraProviderFuture.get()
+
+            // for preview
+            val preview = Preview.Builder().build().also { preview: Preview ->
+                preview.setSurfaceProvider(binding.previvCamera.surfaceProvider)
+            }
+            // for imageCapture
+            val imageCapture = ImageCapture.Builder().build()
 
             // обираємо камеру
-            val cameraSelector = CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                .build()
-
-            // додаємо обработчик подій
-            imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this)){
-                val img: Image = it.image!!
-
-//                val bitmap = mapperYUVtoRGB.translateYUV(img, this)
-                val bitmap = mapperConverotrJava.translateYUV(img, this)
-
-                // дізнаємося у *it* орієнтасію і ставимо у нашого превью таку ж саму
-                binding.imgPhoto.rotation = it.imageInfo.rotationDegrees.toFloat()
-                binding.imgPhoto.setImageBitmap(bitmap)
-                img.close()
-            }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             // головна фун тут, яка запускає все + прив'язка до життєвого сайкл
-            processCameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis)
-
+            try {
+                processCameraProvider.unbindAll()
+                processCameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch (e: Exception) {
+                Log.d("GATS", "exception ---- in initCamera, ${e.message.toString()}")
+            }
         }, ContextCompat.getMainExecutor(this))
-
     }
 
     override fun onDestroy() {
